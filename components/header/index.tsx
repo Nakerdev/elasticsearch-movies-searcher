db207feedf31ddfe8ca680752ff6fdcc558b65b1
Antonio Sánchez <antoniojesussg96@gmail.com>
2021-01-25 22:57:46 +0100

    added header and images

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -3,7 +3,13 @@ import styles from "./header.module.css";
 export const Header = () => {
     return (
         <header className={styles.container}>
-            <h1 className={styles.title}>ElasticMovies</h1>
+            <h1 className={styles.title}>
+                <img className={styles.popcorn} src="/popcorn.png"/>
+                ElasticMovies
+            </h1>
+            <a href="https://github.com/Nakerdev/elasticsearch-movies-searcher" target="_blank">
+                <img className={styles.github} src="/github-logo.png"/>
+            </a>
         </header>
     );
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import '../styles/globals.css'
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Header></Header>
       <Component {...pageProps} />
     </>
   )
