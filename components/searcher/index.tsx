f355927149ffbb64f0c09e7e18f63680195d1e85
Antonio Sánchez <antoniojesussg96@gmail.com>
2021-01-25 22:26:03 +0100

    change searcher styles

--- components/searcher/index.tsx
+++ components/searcher/index.tsx
@@ -3,8 +3,7 @@ import styles from "./searcher.module.css";
 export const Searcher = () => {
     return (
         <form className={styles.container}>
-            <input type="text" className={styles.searchInput} />
-            <button className={styles.searchButton}>Buscar</button>
+            <input type="text" className={styles.input} placeholder="The Movie name" />
         </form >
     );
 };
