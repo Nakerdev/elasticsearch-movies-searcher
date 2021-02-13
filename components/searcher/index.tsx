3f8ac68154d48cd8aca49900ecdf62efa452ac04
Antonio Sánchez <antoniojesussg96@gmail.com>
2021-02-13 20:14:28 +0100

    added magnifying glass as button

--- components/searcher/index.tsx
+++ components/searcher/index.tsx
@@ -4,6 +4,7 @@ export const Searcher = () => {
     return (
         <form className={styles.container}>
             <input type="text" className={styles.input} placeholder="The Movie name" />
+            <input type="image" className={styles.searchButton} src="/search.svg" alt="a magnifying glass"/>
         </form >
     );
 };
