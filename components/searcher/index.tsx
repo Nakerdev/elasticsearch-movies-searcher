5dc75550249829f7720e046034d15694c515e162
Antonio Sánchez <antoniojesussg96@gmail.com>
2021-02-13 21:04:49 +0100

    created markup for the searcing result container

--- components/searcher/index.tsx
+++ components/searcher/index.tsx
@@ -1,10 +1,26 @@
 import styles from "./searcher.module.css";
+import { Children } from "react";
 
 export const Searcher = () => {
     return (
-        <form className={styles.container}>
-            <input type="text" className={styles.input} placeholder="The Movie name" />
-            <input type="image" className={styles.searchButton} src="/search.svg" alt="a magnifying glass"/>
-        </form >
+        <section className={styles.container}>
+            <form className={styles.searcherContainer}>
+                <input type="text" className={styles.searchInput} placeholder="The Movie name" />
+                <input type="image" className={styles.searchButton} src="/search.svg" alt="a magnifying glass"/>
+            </form >
+            <div className={styles.moviesSearchingResultContainer}>
+                <SearchingResult movieTitle="HangOver"></SearchingResult>
+                <SearchingResult movieTitle="Harry Potter"></SearchingResult>
+                <SearchingResult movieTitle="The Terminal"></SearchingResult>
+            </div>
+        </section>
     );
 };
+
+type SearchingResult = {
+    movieTitle: string;
+}
+
+const SearchingResult = ({ movieTitle } : SearchingResult ) => {
+    return <button className={styles.searchingResult}>{movieTitle}</button>
+}
