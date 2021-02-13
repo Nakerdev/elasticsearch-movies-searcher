4753c41b917d96bc4709cdbe6b1e45af1baedb93
Antonio Sánchez <antoniojesussg96@gmail.com>
2021-02-13 20:19:10 +0100

    deleted pop corn image

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -4,7 +4,6 @@ export const Header = () => {
     return (
         <header className={styles.container}>
             <h1 className={styles.title}>
-                <img className={styles.popcorn} src="/popcorn.png"/>
                 ElasticMovies
             </h1>
             <a href="https://github.com/Nakerdev/elasticsearch-movies-searcher" target="_blank">
