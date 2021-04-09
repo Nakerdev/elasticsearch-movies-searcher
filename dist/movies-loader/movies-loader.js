a87306df49f3e0f21b96b2dc1c28a53a0359f59e
Antonio Sánchez <antoniojesussg96@gmail.com>
2021-04-09 22:51:48 +0200

    deleted dist folder

--- dist/movies-loader/movies-loader.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.moviesLoader = void 0;
-function moviesLoader(movieJsonRepository, movieElasticSearchRepository) {
-    var movies = movieJsonRepository.searchAll();
-    movieElasticSearchRepository.create(movies);
-}
-exports.moviesLoader = moviesLoader;
--- dist/movies-loader/runner.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var movies_loader_1 = require("./movies-loader");
-var movieJsonRepository_1 = require("./repositories/movieJsonRepository");
-var movieElasticSearchRepository_1 = require("./repositories/movieElasticSearchRepository");
-function runLoader() {
-    movies_loader_1.moviesLoader(movieJsonRepository_1.movieJsonRepository(), movieElasticSearchRepository_1.movieElasticSearchRepository());
-}
-runLoader();
