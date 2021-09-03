ed56a3ee9396ef8ad116088c59b168ccc952d39c
Antonio Sánchez <antoniojesussg96@gmail.com>
2021-09-03 23:45:55 +0200

    code formatting

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,57 +3,57 @@ import { AppProvider } from "../components/appProvider/index";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-      <>
-    <AppProvider>
-      <Component {...pageProps} />
-      
-    </AppProvider>
-<style jsx global>{`
-html,
-      body,
-      body > div:first-child,
-      div#__next,
-      div#__next > div {
-        padding: 0;
-  margin: 0;
+    <>
+      <AppProvider>
+        <Component {...pageProps} />
+      </AppProvider>
+      <style jsx global>{`
+        html,
+        body,
+        body > div:first-child,
+        div#__next,
+        div#__next > div {
+          padding: 0;
+          margin: 0;
 
-        height: 100%;
-      }
+          height: 100%;
+        }
 
-a {
-  color: inherit;
-  text-decoration: none;
-}
+        a {
+          color: inherit;
+          text-decoration: none;
+        }
 
-h1 {
-  margin: 0;
-}
+        h1 {
+          margin: 0;
+        }
 
-* {
-  box-sizing: border-box;
-  font-family: Arial, sans-serif;
-}tml,
-body {
-  padding: 0;
-  margin: 0;
-  height: 100%;
-}
+        * {
+          box-sizing: border-box;
+          font-family: Arial, sans-serif;
+        }
+        tml,
+        body {
+          padding: 0;
+          margin: 0;
+          height: 100%;
+        }
 
-a {
-  color: inherit;
-  text-decoration: none;
-}
+        a {
+          color: inherit;
+          text-decoration: none;
+        }
 
-h1 {
-  margin: 0;
-}
+        h1 {
+          margin: 0;
+        }
 
-* {
-  box-sizing: border-box;
-  font-family: Arial, sans-serif;
-}
+        * {
+          box-sizing: border-box;
+          font-family: Arial, sans-serif;
+        }
       `}</style>
-      </>
+    </>
   );
 }
 
